90c9e255eccbea997e2875d5ef028bb36465dcec
GayaBala246 <gayabala246@gmail.com>
2021-09-20 22:44:25 +1000

    uploading the data csv file

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,9 @@
 <?php
     
+    function capitalize_word($str) {
+        return ucfirst(strtolower(preg_replace('/[^a-zA-z]/','',$str)));
+    }
+    
     //Database connection 
     $host_name = readline('Enter Host Name: ');
     $db_user = readline('Enter Database User: ');
@@ -15,7 +19,47 @@
         exit();
     }
 
+    $allowed =  array('csv');
+    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
     
-    
+    if(!in_array($ext,$allowed) ) {
+        echo "Please Upload Files With .CSV Extenion Only!";
+    }
+    else {
+        if(file_exists($file_name)) {
+            //Open the csv file for reading
+            $file = fopen($file_name, "r");     
+        
+            //Read the first line(header) and ignore it
+            fgets($file);
+
+            $deleterecords = "TRUNCATE TABLE users"; //empty the table of its current records
+            mysqli_query($mysqli, $deleterecords);
+
+            while (($data = fgetcsv($file, 10000, ",")) !== FALSE) {
+                
+                $name = $data[0];  
+                $sur_name = $data[1];
+                $email = $data[2];
+                $validate_email ="";
+
+                //Validation for name and surname
+                $validate_name = capitalize_word($name);
+                $validate_surname = capitalize_word($sur_name);            
+
+                //Validation for email format
+                if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $validate_email = $email;
+                }               
+
+                $sql = "INSERT into users(name,surname,email) values('$validate_name','$validate_surname','$validate_email')";
+                mysqli_query($mysqli, $sql);
+            }
+        }
+        else {
+            echo "File is not existing in the main project";
+        }
+    }
+        
 
 ?>
