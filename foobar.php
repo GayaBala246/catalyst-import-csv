cfbde6ffcac689ce1026739958148f29d1d1ad02
GayaBala246 <gayabala246@gmail.com>
2021-09-20 22:48:52 +1000

    adding the code implementation

---
+++ foobar.php
@@ -0,0 +1,31 @@
+<?php
+
+$number = 1;
+while($number <= 100)
+{
+    if($number %3 == 0 && $number %5 == 0)
+    {
+        echo "foobar";
+    }
+    elseif($number %3 == 0)
+    {
+        echo "foo";
+    }
+    elseif($number %5 == 0)
+    {
+        echo "bar";
+    }
+    else
+    {
+        echo $number;   
+    }
+    if($number !== 100)
+    {
+        echo ",";
+    }
+    $number ++;
+    
+       
+}
+
+?>
